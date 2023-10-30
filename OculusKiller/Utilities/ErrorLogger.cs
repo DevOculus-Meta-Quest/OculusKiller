981009cf4b739880b93ec78106916ca9df741bb9
Eliminater74 <eliminater74@gmail.com>
2023-10-30 11:28:09 -0400

    Added ErrorLogger Class

---
+++ OculusKiller/Utilities/ErrorLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OculusKiller.Utilities
+{
+    public static class ErrorLogger
+    {
+        private static string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "OculusKiller", "OculusKiller.log");
+
+        public static void Log(string message)
+        {
+            InitializeLogging();
+            File.AppendAllText(logPath, DateTime.Now + ": " + message + "\n");
+        }
+
+        public static void LogError(Exception exception, bool isCritical = false)
+        {
+            string errorMessage = $"An error occurred: {exception.Message}";
+            Log(errorMessage);
+
+            if (isCritical)
+            {
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void InitializeLogging()
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(logPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(logPath));
+            }
+        }
+    }
+}
