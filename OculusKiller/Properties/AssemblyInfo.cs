fa5b4338b0ef7c17f4f2c59620b792a172f089ca
Eliminater74 <eliminater74@gmail.com>
2023-10-30 11:52:57 -0400

    Bump Version 2.0.0.0 Fullly ReWritten

--- OculusKiller/Properties/AssemblyInfo.cs
+++ OculusKiller/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PureFusion")]
 [assembly: AssemblyProduct("Oculus Killer")]
-[assembly: AssemblyCopyright("Copyright © Eliminater74")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("PureFusion By Eliminater74")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
