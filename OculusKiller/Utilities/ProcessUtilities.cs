2e0168aa0a1bfce0e052af09e75a10946f3eb406
Eliminater74 <eliminater74@gmail.com>
2023-10-30 11:44:49 -0400

    Added ProcessUtilities Class, To start Process

---
+++ OculusKiller/Utilities/ProcessUtilities.cs
@@ -0,0 +1,31 @@
+﻿using OculusKiller.Utilities;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace OculusKiller.Utilities
+{
+    public static class ProcessUtilities
+    {
+        public static Process StartProcess(string processPath)
+        {
+            try
+            {
+                Process process = Process.Start(processPath);
+                if (process == null || process.HasExited)
+                {
+                    ErrorLogger.LogError(new Exception($"Failed to start process: {processPath}"));
+                    return null;
+                }
+
+                ErrorLogger.Log($"Successfully started process: {processPath}");
+                return process;
+            }
+            catch (Exception e)
+            {
+                ErrorLogger.LogError(new Exception($"Error starting process: {processPath}. Error: {e.Message}"));
+                return null;
+            }
+        }
+    }
+}
