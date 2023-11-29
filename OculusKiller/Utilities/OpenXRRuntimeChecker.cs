c1590dbe31c4ed54eff9b93dec41842651567b4b
Eliminater74 <eliminater74@gmail.com>
2023-11-29 14:13:14 -0500

    OpenXRRuntimeChecker:
    ### Explanation:
    1. **CheckAndLogRuntimes Method:** This method is the entry point for checking and logging the OpenXR runtimes. It logs available runtimes and the active runtime.
    
    2. **GetAvailableRuntimes Method:** This method retrieves the list of available OpenXR runtimes from the registry.
    
    3. **GetActiveRuntime Method:** This method fetches the currently active OpenXR runtime from the registry.
    
    4. **Error Handling:** The code includes try-catch blocks to handle any exceptions that might occur while accessing the registry and logs them using `ErrorLogger`.
    
    5. **Registry Access:** The `Registry.LocalMachine.OpenSubKey` method is used to access the specified registry keys.

---
+++ OculusKiller/Utilities/OpenXRRuntimeChecker.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using OculusKiller.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace OculusKiller.RuntimeManagement
+{
+    public static class OpenXRRuntimeChecker
+    {
+        private const string OpenXRKeyPath = @"SOFTWARE\Khronos\OpenXR\1";
+
+        public static void CheckAndLogRuntimes()
+        {
+            try
+            {
+                // Log available runtimes
+                var availableRuntimes = GetAvailableRuntimes();
+                ErrorLogger.Log("Available OpenXR Runtimes:");
+                foreach (var runtime in availableRuntimes)
+                {
+                    ErrorLogger.Log(runtime);
+                }
+
+                // Log active runtime
+                var activeRuntime = GetActiveRuntime();
+                ErrorLogger.Log($"Active OpenXR Runtime: {activeRuntime}");
+            }
+            catch (Exception e)
+            {
+                ErrorLogger.LogError(e);
+            }
+        }
+
+        private static List<string> GetAvailableRuntimes()
+        {
+            var runtimes = new List<string>();
+            using (var key = Registry.LocalMachine.OpenSubKey($@"{OpenXRKeyPath}\AvailableRuntimes"))
+            {
+                if (key != null)
+                {
+                    foreach (var valueName in key.GetValueNames())
+                    {
+                        var runtimePath = key.GetValue(valueName) as string;
+                        if (!string.IsNullOrEmpty(runtimePath))
+                        {
+                            runtimes.Add(runtimePath);
+                        }
+                    }
+                }
+            }
+            return runtimes;
+        }
+
+        private static string GetActiveRuntime()
+        {
+            using (var key = Registry.LocalMachine.OpenSubKey(OpenXRKeyPath))
+            {
+                if (key != null)
+                {
+                    return key.GetValue("ActiveRuntime") as string;
+                }
+            }
+            return "No active runtime found.";
+        }
+    }
+}
